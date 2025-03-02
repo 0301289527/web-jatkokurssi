8c7d14ba9a1a467bacc4a297d9d8e2fd445a6a50
Yuxuan Liu <liu.yuxuan2003@outlook.com>
2025-03-02 21:22:59 +0200

    serverExpress.js

--- web-jatko/StaticWebApp/serverExpress.js
+++ web-jatko/StaticWebApp/serverExpress.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const http = require('http');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Define your routes and middleware here
+const server = http.createServer(app); // 确保 server 变量被定义
 
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost: ${PORT}`);
-      });
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:MACHINE-IP-ADDRESS:${PORT}`);
+});
